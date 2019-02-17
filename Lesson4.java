96c6b0c761b417234031761306be55c7dd984dd6
Dmitry <rdi81@yandex.ru>
2019-02-17 14:35:05 +0300

    Задачи 3,4 логика не доработана: при заполненности строк и отсутствии нормального продолжения игры зависает в бесконечности

--- Lesson4.java
+++ Lesson4.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class Lesson4 {
 
     public static char[][] map;
-    public static final int SIZE = 3;
-    public static final int DOT_TO_WIN = 3;
+    public static final int SIZE = 5;
+    public static final int DOT_TO_WIN = 4;
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
     public static final char DOT_EMPTY = '*';
     public static Scanner sc = new Scanner(System.in);
     public static Random rand = new Random();
+    public static int volueOfWinCombination = SIZE /*выигрыш по строкам*/ +
+            +SIZE /*выигрыш по столбцам*/ +
+            +2 /*по диагоналям*/ + (SIZE - DOT_TO_WIN) * 2 + 2 /*выигрыш по другим диагоналям*/;
+    public static int[] win = new int[volueOfWinCombination];
 
     public static void main(String[] args) {
         initMap();
@@ -37,9 +41,7 @@ public class Lesson4 {
                 System.out.println("Ничья");
                 break;
             }
-
         }
-
     }
 
     private static boolean checkFull() {
@@ -85,12 +87,91 @@ public class Lesson4 {
     }
 
     public static void compTurn() {
-        int x, y;
+        int x = 0, y = 0;
+        int maxZnachenieVeroyatnosty = 0;
+        int pozichiyaMaxZnachenieVeroyatnosty = 0;
+        for (int i = 0; i < volueOfWinCombination; i++) {
+            if (win[i] > maxZnachenieVeroyatnosty) {
+                maxZnachenieVeroyatnosty = win[i];
+                pozichiyaMaxZnachenieVeroyatnosty = i + 1;
+            }
+        }
         do {
-            x = rand.nextInt(SIZE);
-            y = rand.nextInt(SIZE);
+            // находим по какому выигрышу максимальная вероятноть
+            switch (pozichiyaMaxZnachenieVeroyatnosty) {
+//блокировка по строкам
+                case 1:
+                    y = pozichiyaMaxZnachenieVeroyatnosty - 1;
+                    x = rand.nextInt(SIZE);
+                    break;
+                case 2:
+                    y = pozichiyaMaxZnachenieVeroyatnosty - 1;
+                    x = rand.nextInt(SIZE);
+                    break;
+                case 3:
+                    y = pozichiyaMaxZnachenieVeroyatnosty - 1;
+                    x = rand.nextInt(SIZE);
+                    break;
+                case 4:
+                    y = pozichiyaMaxZnachenieVeroyatnosty - 1;
+                    x = rand.nextInt(SIZE);
+                    break;
+                case 5:
+                    y = pozichiyaMaxZnachenieVeroyatnosty - 1;
+                    x = rand.nextInt(SIZE);
+                    break;
+                // блокировка по столбцам
+                case 6:
+                    x = pozichiyaMaxZnachenieVeroyatnosty - 1 - SIZE;
+                    y = rand.nextInt(SIZE);
+                    break;
+                case 7:
+                    x = pozichiyaMaxZnachenieVeroyatnosty - 1 - SIZE;
+                    y = rand.nextInt(SIZE);
+                    break;
+                case 8:
+                    x = pozichiyaMaxZnachenieVeroyatnosty - 1 - SIZE;
+                    y = rand.nextInt(SIZE);
+                    break;
+                case 9:
+                    x = pozichiyaMaxZnachenieVeroyatnosty - 1 - SIZE;
+                    y = rand.nextInt(SIZE);
+                    break;
+                case 10:
+                    x = pozichiyaMaxZnachenieVeroyatnosty - 1 - SIZE;
+                    y = rand.nextInt(SIZE);
+                    break;
+//блокировка по диагонали
+                case 11:
+                    x = y = rand.nextInt(SIZE);
+                    break;
+                case 12:
+                    y = rand.nextInt(SIZE);
+                    x = SIZE - y - 1;
+                    break;
+//блокировка по другим диагоналям
+// по паралельным главной диагонали
+                case 13:
+                    x = rand.nextInt(SIZE);
+                    y = x - 1;
+                    break;
+                case 14:
+                    y = rand.nextInt(SIZE - 1);
+                    x = y - 1;
+                    break;
+// по паралельным другой диагонали
+                case 15:
+                    y = rand.nextInt(SIZE - 1);
+                    x = SIZE - y - 2;
+                    break;
+                case 16:
+                    x = rand.nextInt(SIZE) + 1;
+                    y = SIZE - x;
+                    break;
+            }
         } while (isCellValid(x, y));
-        System.out.println("Комп походил в точку " + (x + 1) + " " + (y + 1));
+        System.out.println("Комп походил в точку " + (x + 1) + " " + (y + 1) /*+
+                +maxZnachenieVeroyatnosty + "  " + pozichiyaMaxZnachenieVeroyatnosty*/);
         map[y][x] = DOT_O;
     }
 
@@ -100,42 +181,88 @@ public class Lesson4 {
         return true;
     }
 
-    public static boolean checkWin(char symb) {
-        int volueOfWinCombination = SIZE /*выигрыш по строкам*/ +
-                +SIZE /*выигрыш по столбцам*/ +
-                + 2 /*по диагоналям*/ + (SIZE - DOT_TO_WIN) * 2 /*выигрыш по другим диагоналям*/;
-        int[] win = new int[volueOfWinCombination];
+    public static boolean checkWin(char symb) { //проверка выигрыша и расчет вероятности
+        for (int i = 0; i < volueOfWinCombination; i++) {
+            win[i] = 0;
+        }
 
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 if (map[i][j] == symb) {
-                    win[i] += 1;
-                    win[j + SIZE] += 1;
-                    if (i == j) win[SIZE + SIZE] += 1;
-                    if (i == SIZE - 1 - j) win[SIZE + SIZE + 1] += 1;
-                    // if ((SIZE - DOT_TO_WIN)>0)
+
+                    //"вероятность" победы по строке
+                    win[i] += (j == 0 || j == SIZE - 1) ? 9 : 10;
+
+                    //"вероятность" победы по столбцу
+                    win[j + SIZE] += (i == 0 || i == SIZE - 1) ? 9 : 10;
+
+                    //"вероятность" победы по диагонали
+                    if ((i == j) && (i == 0 || i == SIZE - 1)) win[SIZE + SIZE] += 9;
+                    else if (i == j) win[SIZE + SIZE] += 10;
+
+                    //"вероятность" победы по диагонали справа вниз
+                    if (i == (SIZE - 1 - j) && (i == 0 || i == SIZE - 1)) win[SIZE + SIZE + 1] += 9;
+                    else if (i == (SIZE - 1 - j)) win[SIZE + SIZE + 1] += 10;
+
+                    /*выигрыш по другим диагоналям*/
+                    if ((SIZE - DOT_TO_WIN) > 0) {
+                        //над диагональю
+                        if (i == (j - 1)) win[SIZE + SIZE + 2] += 10;
+                        //над диагональю
+                        if ((i - 1) == j) win[SIZE + SIZE + 3] += 10;
+
+                        //над диагональю справа вниз
+                        if (i == (SIZE - 2 - j)) win[SIZE + SIZE + 4] += 10;
+                        //под диагональю справа вниз
+                        if (i == (SIZE - j)) win[SIZE + SIZE + 5] += 10;
+                    }
+
+
+                } else if (map[i][j] == DOT_EMPTY) {
+                } //если ячейка пустая, то ничего
+
+//если ячейка занята не нашей фишкой вычитаем вероятность выигрыша
+                //если ячейки заняты не нашей фишкой, и это сказывается на возможности выигрыша (к примеру 0.0 не кретично
+                else {
+
+                    //"вероятность" победы по строке
+                    win[i] -= (j == 0 || j == SIZE - 1) ? 0 : 50;
+                    win[i] -= ((j == 0 || j == SIZE - 1) && win[i] > 30) ? 50 : 0;
+
+                    //"вероятность" победы по столбцу
+                    win[j + SIZE] -= (i == 0 || i == SIZE - 1) ? 0 : 50;
+                    win[j + SIZE] -= ((i == 0 || i == SIZE - 1) && win[j + SIZE] > 30) ? 50 : 0;
+
+                    //"вероятность" победы по диагонали
+
+                    if ((i == j) && !(i == 0 || i == SIZE - 1)) win[SIZE + SIZE] -= 50;
+                    else if ((i == j) && (i == 0 || i == SIZE - 1) && win[SIZE + SIZE] > 30) win[SIZE + SIZE] -= 50;
+
+                    //"вероятность" победы по диагонали справа вниз
+                    if (i == (SIZE - 1 - j) && !(i == 0 || i == SIZE - 1)) win[SIZE + SIZE + 1] -= 50;
+                    else if ((i == (SIZE - 1 - j)) && (i == 0 || i == SIZE - 1) && win[SIZE + SIZE + 1] > 30)
+                        win[SIZE + SIZE + 1] -= 50;
+
+                    if ((SIZE - DOT_TO_WIN) > 0) {
+                        //над диагональю
+                        if (i == (j - 1)) win[SIZE + SIZE + 2] -= 50;
+                        //под диагональю
+                        if ((i - 1) == j) win[SIZE + SIZE + 3] -= 50;
+                        //над диагональю справа вниз
+                        if (i == (SIZE - 2 - j)) win[SIZE + SIZE + 4] -= 50;
+                        //под диагональю справа вниз
+                        if (i == (SIZE - j)) win[SIZE + SIZE + 5] -= 50;
+                    }
                 }
+
             }
         }
         for (int i = 0; i < volueOfWinCombination; i++) {
-            if (win[i] == DOT_TO_WIN) return true;
+            System.out.print(win[i] + " ");
+        }
+        for (int i = 0; i < volueOfWinCombination; i++) {
+            if (win[i] >= DOT_TO_WIN * 10 - 1) return true;
         }
         return false;
-
-
     }
-
-//        public static boolean checkWin(char symb) {
-//        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
-//        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
-//        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
-//        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return true;
-//        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return true;
-//        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
-//        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
-//        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
-//        return false;
-//    }
-
-
 }
